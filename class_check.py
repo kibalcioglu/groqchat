7d8baceef580564e81f70e0e95a24b829ca9ecaa
kibalcioglu <k.i.balcioglu@icloud.com>
2025-06-22 00:43:27 +0300

    Delete class_check.py

--- class_check.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-
-jar_path = "zemberek-full.jar"
-
-with zipfile.ZipFile(jar_path, "r") as jar:
-    print(f"📦 İçindekiler: {jar_path}")
-    for name in jar.namelist():
-        if name.endswith(".class") and "morphology" in name:
-            print(name)
